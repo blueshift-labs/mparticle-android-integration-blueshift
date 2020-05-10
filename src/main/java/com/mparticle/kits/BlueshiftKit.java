6c8bf451add6872b7e8a86a32b0e8a254d1b2e9c
Rahul Raveendran V P <rahul.pmna@gmail.com>
2020-05-10 22:14:10 +0530

    removed sample app and configured bintray upload

--- src/main/java/com/mparticle/kits/BlueshiftKit.java
+++ src/main/java/com/mparticle/kits/BlueshiftKit.java
@@ -5,8 +5,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.blueshift.Blueshift;
 import com.blueshift.BlueshiftConstants;
